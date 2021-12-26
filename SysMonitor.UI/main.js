1d5388b7f3dbb81c57392779c3eec5adf334feea
Adam Barry-O'Donovan <adambarryodonovan@gmail.com>
2021-12-26 16:52:09 +0000

    Setup Installer

--- SysMonitor.UI/main.js
+++ SysMonitor.UI/main.js
@@ -3,8 +3,6 @@ const { ipcMain, dialog, globalShortcut, systemPreferences } = require("electron
 const url = require("url");
 const path = require("path");
 const net = require("net");
-const ipc = require("node-ipc");
-const NamedPipes = require("named-pipes");
 
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
@@ -35,35 +33,6 @@ app.on("ready", () => {
 
 
     try {
-        // const client = net.createConnection(PIPE_PATH, () => {
-        //     console.log('connected to server!');
-        // });
-
-        // var server = net.createServer(function(stream) {
-        //     console.log('Server: on connection')
-        
-        //     stream.on('data', function(c) {
-        //         console.log('Server: on data:', c.toString());
-        //     });
-        
-        //     stream.on('end', function() {
-        //         console.log('Server: on end')
-        //         server.close();
-        //     });
-        
-        //     stream.write('Take it easy!');
-        // });
-
-        // server.on('close',function(){
-        //     console.log('Server: on close');
-        // })
-        
-        // server.listen(PIPE_PATH,function(){
-        //     console.log('Server: on listening');
-        // })
-        
-        
-
         setInterval(() => {
             var client = net.connect(PIPE_PATH, function() {
                 //console.log('Client: on connection');
@@ -84,48 +53,8 @@ app.on("ready", () => {
                 console.log (e)
             })
         }, 1000);
-
-        //var pipe = NamedPipes.connect(PIPE_NAME);
-
-        // ipc.connectTo(PIPE_PATH, () => {
-        //     ipc.of.PIPE_PATH.on(
-        //         'connect',
-        //         function(){
-        //             ipc.log('## connected to world ##', ipc.config.delay);
-        //             ipc.of.world.emit(
-        //                 'app.message',
-        //                 {
-        //                     id      : ipc.config.id,
-        //                     message : 'hello'
-        //                 }
-        //             );
-        //         }
-        //     );
-        //     ipc.of.PIPE_PATH.on(
-        //         'disconnect',
-        //         function(){
-        //             ipc.log('disconnected from world');
-        //         }
-        //     );
-        //     ipc.of.PIPE_PATH.on(
-        //         'app.message',
-        //         function(data){
-        //             ipc.log('got a message from world : ', data);
-        //         }
-        //     );
-        //     ipc.of.PIPE_PATH.on(
-        //         'kill.connection',
-        //         function(data){
-        //             ipc.log('world requested kill.connection');
-        //             ipc.disconnect('world');
-        //         }
-        //     );
-        // });
     }
     catch (e) {
         console.log (e)
     }
-    //pipe = NamedPipes.connect('SysMonitor.Service.Pipe');
-
-    // var pipe = NamedPipes.connect('SysMonitor.Service.Pipe');
 });
