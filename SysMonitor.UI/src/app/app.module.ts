30846b71657bd4dc51c9b1ed5cc2b80ca8cf08df
AdamBOD <adambarryodonovan@gmail.com>
2022-01-05 17:55:51 +0000

    Restructured project

--- SysMonitor.UI/src/app/app.module.ts
+++ SysMonitor.UI/src/app/app.module.ts
@@ -3,19 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SparklineComponent } from './sparkline/sparkline.component';
-import { FanComponent } from './fan/fan.component';
-import { FansContainerComponent } from './fans-container/fans-container.component';
-import { GpuContainerComponent } from './gpu-container/gpu-container.component';
-import { GpuComponent } from './gpu/gpu.component';
+import { FanComponent } from './components/fans-container/fan/fan.component';
+import { FansContainerComponent } from './components/fans-container/fans-container.component';
+import { GpuContainerComponent } from './components/gpu-container/gpu-container.component';
+import { GpuImageComponent } from './components/gpu-container/gpu-image/gpu-image.component';
+import { SparklineComponent } from './components/sparkline/sparkline.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         FanComponent,
         FansContainerComponent,
-        GpuComponent,
         GpuContainerComponent,
+        GpuImageComponent,
         SparklineComponent
     ],
     imports: [
---
+++ SysMonitor.UI/src/app/components/gpu-container/gpu-image/gpu-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-gpu-image',
+    templateUrl: './gpu-image.component.html',
+    styleUrls: ['./gpu-image.component.scss']
+})
+export class GpuImageComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
