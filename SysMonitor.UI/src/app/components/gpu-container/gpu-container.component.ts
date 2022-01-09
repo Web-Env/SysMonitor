d2d35005a0071c660939109a16d0a0834172b012
AdamBOD <adambarryodonovan@gmail.com>
2022-01-09 21:08:18 +0000

    Update gpu-container.component.ts

--- SysMonitor.UI/src/app/components/gpu-container/gpu-container.component.ts
+++ SysMonitor.UI/src/app/components/gpu-container/gpu-container.component.ts
@@ -1,37 +1,17 @@
-import { ChangeDetectorRef, Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { gpu } from "src/app/models/gpu.model";
-import { GpuImageComponent } from "./gpu-image/gpu-image.component";
 
 @Component({
     selector: 'app-gpu-container',
     templateUrl: './gpu-container.component.html',
     styleUrls: ['./gpu-container.component.scss']
 })
-export class GpuContainerComponent implements OnChanges, OnInit {
+export class GpuContainerComponent implements OnInit {
     @Input() gpu!: gpu;
 
-    @ViewChild(GpuImageComponent)
-    private gpuImage!: GpuImageComponent;
-
-    fanRpm!: number;
-
-    constructor(private changeDetectorRef: ChangeDetectorRef) { }
+    constructor() { }
 
     ngOnInit(): void {
-    }
-
-    ngOnChanges(changes: SimpleChanges): void {
-        console.log(changes['gpu'].currentValue.FanRpm)
-        //this.gpuImage.updateFanSpeed(changes['gpu'].currentValue.FanRpm);
-    }
-
-    public updateData(gpuData: gpu) {
-        // this.gpu = gpuData;
-        // this.fanRpm = this.gpu.FanRpm;
-        // console.log (this.fanRpm)
-
-        
-        //this.changeDetectorRef.detectChanges();
     }
 
 }
